17ba5bdfe400a32622dd5e3cd381e5fa12533e12
Iwen <gendro_i@epita.fr>
2018-09-03 10:43:35 +0200

    Improved script, add manual input option

--- main.py
+++ main.py
@@ -2,6 +2,23 @@ import glob
 import os
 from shutil import copyfile
 
+
+def confirmation(in_folder, out_folder, file_to_copy, student_login):
+    user_command = input("Copy " + file_to_copy + " to " + out_folder
+                         + student_login + ".jpg ? y/c to enter file manually \n")
+    if user_command == "y":
+        copyfile(file_to_copy, out_folder + student_login + ".jpg")  # copy image from in folder to out folder
+        print("File renamed")
+    elif user_command == "c":
+        file_to_copy = in_folder + input("Correct file to copy in input folder : \n")
+        if os._exists(file_to_copy):
+            confirmation(in_folder, out_folder, file_to_copy, student_login)
+        else:
+            print("File do not exist")
+    else:
+        print("File note renamed")
+
+
 input_folder = input("Enter path to input folder \n")
 output_folder = input("Enter path to output folder \n")
 if not (os.path.isdir(output_folder)):
@@ -10,13 +27,7 @@ if not (output_folder.endswith("/") or output_folder.endswith("\\")):  # To be s
     output_folder += "/"
 if not (input_folder.endswith("/") or input_folder.endswith("\\")):
     input_folder += "/"
-while (True):
+while True:
     student_login = input("Enter student login \n")
     list_of_files = glob.iglob(input_folder + "*.jpg")  # select all images from folder
-    latest_file = max(list_of_files, key=os.path.getctime)  # select last created file
-    confirmation = input("Copy " + latest_file + " to " + output_folder + student_login + ".jpg ? y \n")
-    if (confirmation == "y"):
-        copyfile(latest_file, output_folder + student_login + ".jpg")  # copy image from in folder to out folder while renaming it
-        print("File renamed")
-    else:
-        print("File note renamed")
+    confirmation(input_folder, output_folder, max(list_of_files, key=os.path.getctime), student_login)
